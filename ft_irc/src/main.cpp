1d038a668b6a321d830dd8e6b1caca545cca3552
MrSloth-dev <joao-pol@student.42porto.com>
2025-05-06 16:04:21 +0100

    Makefile start with generic file fetching, need to fix relink

---
+++ ft_irc/src/main.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+
+void Println(std::string str);
+int main() {
+  std::cout << "Let's Fucking go!" << std::endl;
+  Println("Less words are fun(ctions)");
+}
---
+++ ft_irc/src/utils/UtilsFun.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+#include <string>
+void Println(std::string str) {
+  std::cout << str << std::endl;
+
+}
