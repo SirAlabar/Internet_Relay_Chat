dd869d4cc31e67a57df00157f548bb7f0e42091c
MrSloth-dev <joao8barbosa@gmail.com>
2025-05-15 15:08:57 +0100

    print class

--- ft_irc/src/utils/UtilsFun.cpp
+++ ft_irc/src/utils/UtilsFun.cpp
@@ -1,22 +1,25 @@
+#include "utils/UtilsFun.hpp"
 #include <iostream>
 
-#include "utils/UtilsFun.hpp"
-
-void Print::Debug(const std::string& str)
-{
-    if (DEBUG) std::cerr << RED << "[DEBUG]: " << RESET << str << std::endl;
+void Print::Debug(const std::string& str) {
+    std::cerr << RED << "[DEBUG]: " << RESET << str << std::endl;
 }
 
-void Print::Log(const std::string& str)
-{
-    if (LOG) std::cerr << RED << "[LOG]: " << RESET << str << std::endl;
+void Print::Log(const std::string& str) {
+    std::cerr << RED << "[LOG]: " << RESET << str << std::endl;
 }
 
-void Print::StdOut(const std::string& str) { std::cout << str << std::endl; }
+void Print::StdOut(const std::string& str) {
+    std::cout << str << std::endl;
+}
 
-void Print::StdErr(const std::string& str) { std::cerr << str << std::endl; }
+void Print::StdErr(const std::string& str) {
+    std::cerr << str << std::endl;
+}
 
-void Print::Stream(std::ofstream& os, const std::string& str) { os << str << std::endl; }
+void Print::Stream(std::ofstream& os, const std::string& str) {
+    os << str << std::endl;
+}
 
 // Private constructor definition (optional, but good practice)
 Print::Print() {}
