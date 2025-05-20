2f56faecd7d02fe77271c3bb14443d120bb610db
MrSloth-dev <joao8barbosa@gmail.com>
2025-05-21 00:04:38 +0100

    started to deepen the parser

--- ft_irc/src/core/Message.cpp
+++ ft_irc/src/core/Message.cpp
@@ -3,6 +3,8 @@
 
 #include "Message.hpp"
 #include "UtilsFun.hpp"
+#include <sstream>
+#include <string>
 
 Message::Message(const std::string& rawMessage)
 {
@@ -22,7 +24,7 @@ Message::Message(const std::string& rawMessage)
 
 Message::~Message() {}
 
-const std::string& Message::getCommand() const { return _command; }
+const std::string &Message::getCommand() const { return _command; }
 
 const std::vector<std::string> Message::getParams() const { return _params; }
 const std::string Message::getParams(size_t i) const
