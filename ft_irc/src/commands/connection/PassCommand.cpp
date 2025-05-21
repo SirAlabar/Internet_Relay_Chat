513868794891a8a9f1455e75c330f066b4080cb0
isilva-t <ivanrfteixeira@gmail.com>
2025-05-21 15:14:12 +0100

    stupid enhaced log

--- ft_irc/src/commands/connection/PassCommand.cpp
+++ ft_irc/src/commands/connection/PassCommand.cpp
@@ -24,39 +24,46 @@ ACommand* PassCommand::create(Server* server)
 
 void PassCommand::execute(Client* client, const Message& message)
 {
-	Print::Debug("Starting execution of PASS command");
-  
-	if (!client)
-	{
-		Print::Debug("Client is NULL, returning");
-		return ;
-	}
+    Print::Do("execute PASS command");
 
-	std::string password = message.getParams();
-	size_t startPass = password.find_first_not_of(" \t");
-	if (password.empty() || startPass == std::string::npos)
-	{
-		Print::Debug("no password, returning...");
-		sendErrorReply(client, 461, "PASS :Not enough parameters");
-		return ;
-	}
-	
-	password = password.substr(startPass);
-	if (!password.empty() && password[0] == ':')
-		password = password.substr(1);
+    if (!client)
+    {
+        Print::Fail("Client NULL");
+        return ;
+    }
 
-	size_t endPass = password.find_last_not_of(" \t\r\n");
-	if (endPass != std::string::npos)
-		password = password.substr(0, endPass + 1);
+    std::string password = message.getParams();
+    size_t startPass = password.find_first_not_of(" \t");
+    if (password.empty() || startPass == std::string::npos)
+    {
+        Print::Fail("no password");
+        sendErrorReply(client, 461, "PASS :Not enough parameters");
+        return ;
+    }
 
-	if (client->isAuthenticated())
-	{
-		sendErrorReply(client, 462, ":You may not reregister");
-		return ;
-	}
+    password = password.substr(startPass);
+    if (!password.empty() && password[0] == ':')
+        password = password.substr(1);
 
-	if(password == _server->getPassword())
-		client->setAuthenticated(true);
-	else
-		sendErrorReply(client, 464, ":Password incorrect!");
+    size_t endPass = password.find_last_not_of(" \t\r\n");
+    if (endPass != std::string::npos)
+        password = password.substr(0, endPass + 1);
+
+    if (client->isAuthenticated())
+    {
+        Print::Warn("can't register");
+        sendErrorReply(client, 462, ":You may not reregister");
+        return ;
+    }
+
+    if(password == _server->getPassword())
+    {
+        Print::Ok("");
+        client->setAuthenticated(true);
+    }
+    else
+    {
+        Print::Warn("Password incorrect");
+        sendErrorReply(client, 464, ":Password incorrect!");
+    }
 }
