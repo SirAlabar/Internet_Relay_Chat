780ba16b90f1963b0c9bbaff7b7c71bcf0cd550e
MrSloth-dev <joao.a.barbosa@proton.me>
2025-05-22 17:44:55 +0100

    add client and add operator on Channel, not working

--- ft_irc/include/core/Channel.hpp
+++ ft_irc/include/core/Channel.hpp
@@ -9,22 +9,22 @@ class Client;
 class Channel
 {
 private:
-	std::string _name;
-	std::string _topic;
-	std::map<int, Client*> _clients;
-	std::map<int, Client*> _operators;
+    std::string _name;
+    std::string _topic;
+    std::map<int, Client*> _clients;
+    std::map<int, Client*> _operators;
 
 public:
-	Channel(const std::string& name);
-	~Channel();
+    Channel(const std::string& name);
+    ~Channel();
 
-	const std::string& getName() const;
-	const std::string& getTopic() const;
-	void setTopic(const std::string& topic);
-
-	void addClient(Client* client);
-	void removeClient(Client* client);
-	bool isOperator(Client* client) const;
+    const std::string& getName() const;
+    const std::string& getTopic() const;
+    void setTopic(const std::string& topic);
+    void addClient(Client* client);
+    void addOperator(Client* _operator);
+    void removeClient(Client* client);
+    bool isOperator(Client* client) const;
 };
 
 #endif
--- ft_irc/src/core/Channel.cpp
+++ ft_irc/src/core/Channel.cpp
@@ -1,5 +1,6 @@
 #include "Channel.hpp"
 #include "Client.hpp"
+#include "UtilsFun.hpp"
 
 Channel::Channel(const std::string& name) : _name(name) {}
 
@@ -13,7 +14,18 @@ void Channel::setTopic(const std::string& topic) { _topic = topic; }
 
 void Channel::addClient(Client* client)
 {
+    Print::Debug("Map");
+    for (std::map<int, Client*>::iterator ite = _clients.begin(); ite != _clients.end();
+         ite++)
+    {
+        Print::Debug(toString(ite->first) + " " + ite->second->getUsername());
+    }
     if (client) _clients[client->getFd()] = client;
+}
+
+void Channel::addOperator(Client* _operator)
+{
+    if (_operator) _clients[_operator->getFd()] = _operator;
 }
 
 void Channel::removeClient(Client* client)
