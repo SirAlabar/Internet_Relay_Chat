48ad31fd082445aff8030731589b8246b0f9a3b4
isilva-t <ivanrfteixeira@gmail.com>
2025-05-26 15:02:13 +0100

    feat: Topic something is in

--- ft_irc/src/commands/channel/TopicCommand.cpp
+++ ft_irc/src/commands/channel/TopicCommand.cpp
@@ -1,5 +1,6 @@
 #include "TopicCommand.hpp"
 #include "Client.hpp"
+#include "General.hpp"
 #include "Message.hpp"
 #include "Server.hpp"
 #include "Channel.hpp"
@@ -30,5 +31,53 @@ ACommand* TopicCommand::create(Server* server)
 // Execute the TOPIC command
 void TopicCommand::execute(Client* client, const Message& message)
 {
-	// Future implementation
+    Print::Do("execute TOPIC command");
+
+    if(!validateClient(client))
+    {
+        return;
+    }
+    
+    if(!validateParameterCount(client, message, 1, "TOPIC"))
+    {
+        return;
+    }
+
+    std::string channelName = message.getParams(0);
+    Channel* channel = validateAndGetChannel(client, channelName);
+    if (!channel)
+    {
+        return;
+    }
+
+    if (!validateChannelMembership(client, channel, channelName))    
+    {
+        return;
+    }
+
+    if (channel->isTopicRestricted() && !channel->isOperator(client))
+    {
+        Print::Warn("Client isn't operator and topic is restricted");
+        sendErrorReply(client, IRC::ERR_CHANOPRIVSNEEDED, 
+                       channelName + " :You're not channel operator");
+        return;
+    }
+
+    std::string newTopic = message.getParams(1);
+    if (!newTopic.empty())
+    {
+        channel->setTopic(message.getParams(1));
+    }
+
+    std::string topicMsg = ":" + client->getNickname();
+    if (!client->getUsername().empty())
+    {
+        topicMsg += "!" + client->getUsername() + "@localhost"; // need to change that localhost!!!
+        //
+    }
+    topicMsg += " TOPIC " + channelName + " :" + newTopic + "\r\n";
+
+    _server->broadcastChannel(topicMsg, channelName, -1);
+
+    Print::Ok("");
 }
