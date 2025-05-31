107a52663719db6a40ee0e6dc51670e5e4bbfb6b
isilva-t <ivanrfteixeira@gmail.com>
2025-05-31 17:20:38 +0100

    fix: Config limits to 100 lines, and 4096 chars per line

--- ft_irc/src/utils/Config.cpp
+++ ft_irc/src/utils/Config.cpp
@@ -11,50 +11,55 @@ bool    Config::hasConfig(const std::string& wanted)
 
 std::string Config::getConfig(const std::string& wanted)
 {
-	if(wanted.empty())
-	{
-		return("");
-	}
-	const char* config_file = "config.txt";
-	std::string source_file(config_file);
-	std::ifstream file(config_file);
-	if (!file.is_open())
-	{
-		return ("");
-	}
+    if(wanted.empty())
+    {
+        return("");
+    }
+    const char* config_file = "config.txt";
+    std::string source_file(config_file);
+    std::ifstream file(config_file);
+    if (!file.is_open())
+    {
+        return ("");
+    }
 
-	std::string line;
-	while (std::getline(file, line))
-	{
-		line.erase(0, line.find_first_not_of("\t\n\v\f\r "));
-		line.erase(line.find_last_not_of("\t\n\v\f\r ") + 1);
-		if (line.empty() || line[0] == '#' || line.find_first_of("\t\n\v\f\r ") != std::string::npos)
-		{
-			continue;
-		}
-		size_t equalPos = line.find('=');
-		if (equalPos == std::string::npos)
-		{
-			continue;
-		}
-		std::string varName = line.substr(0, equalPos);
-		if (varName != wanted)
-		{
-			continue ;
-		}
-		if(equalPos + 1 < line.length())
-		{
-			file.close();
-			return (line.substr(equalPos + 1));
-		}
-		else
-		{
-			file.close();
-			return("");
-		}
-	}
-	file.close();
-	return("");
+    std::string line;
+    size_t i = 0;
+    while (std::getline(file, line) && ++i < 100)
+    {
+        if (line.length() > 4096)
+        {
+            continue;
+        }
+        line.erase(0, line.find_first_not_of("\t\n\v\f\r "));
+        line.erase(line.find_last_not_of("\t\n\v\f\r ") + 1);
+        if (line.empty() || line[0] == '#' || line.find_first_of("\t\n\v\f\r ") != std::string::npos)
+        {
+            continue;
+        }
+        size_t equalPos = line.find('=');
+        if (equalPos == std::string::npos)
+        {
+            continue;
+        }
+        std::string varName = line.substr(0, equalPos);
+        if (varName != wanted)
+        {
+            continue ;
+        }
+        if(equalPos + 1 < line.length())
+        {
+            file.close();
+            return (line.substr(equalPos + 1));
+        }
+        else
+        {
+            file.close();
+        return("");
+        }
+    }
+    file.close();
+    return("");
 }
 
 Config::Config() {}
